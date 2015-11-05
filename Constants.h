2c482603efda5353edcef3582c351a382d10686a
Hojjat <imani.hojjat95@gmail.com>
2015-11-06 00:22:57 +0330

    initial Commit

---
+++ Constants.h
@@ -0,0 +1,7 @@
+//
+// Created by hojjat on 11/6/15.
+//
+
+const char *SHARED_MEMORY_NAME = "MySharedMemory";
+const int SHARED_MEMORY_SIZE = 4096;
+const int LOOP_SIZE = 100000;
---
+++ P2.cpp
@@ -0,0 +1,61 @@
+//
+// Created by hojjat on 11/5/15.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include "Constants.h"
+
+int main(void) {
+    int shared_memory;
+    char *shm_ptr;
+
+    /* open the shared memory segment as if it was a file */
+    shared_memory = shm_open(SHARED_MEMORY_NAME, O_RDWR, 0666);
+    if (shared_memory == -1) {
+        printf("Shared memory failed: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    /* map the shared memory segment to the address space of the process */
+    shm_ptr = (char *) mmap(0, SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_memory, 0);
+    if (shm_ptr == MAP_FAILED) {
+        printf("Map failed: %s\n", strerror(errno));
+        exit(1);
+    }
+    int value;
+    for (int i = 0; i < LOOP_SIZE; i++) {
+        sscanf(shm_ptr, "%d", &value);
+        value--;
+        sprintf(shm_ptr, "%d", value);
+        printf("P2: %d\n", value);
+    }
+    sscanf(shm_ptr, "%d", &value);
+    printf("P2 done: last value = %d", value);
+    if (value != LOOP_SIZE)
+        printf("     Race Happened!");
+    printf("\n");
+    /* remove the mapped shared memory segment from the address space of the process */
+    if (munmap(shm_ptr, SHARED_MEMORY_SIZE) == -1) {
+        printf("Unmap failed: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    /* close the shared memory segment as if it was a file */
+    if (close(shared_memory) == -1) {
+        printf("Close failed: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    /* remove the shared memory segment from the file system */
+    if (shm_unlink(SHARED_MEMORY_NAME) == -1) {
+        printf("Error removing %s: %s\n", SHARED_MEMORY_NAME, strerror(errno));
+        exit(1);
+    }
+
+    return 0;
+}
